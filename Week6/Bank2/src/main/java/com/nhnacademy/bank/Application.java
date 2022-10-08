bafd7bfb41a1917e1fc6ca4accab122786be6f1d
unhas01 <rudtj2966@gmail.com>
2022-10-08 11:31:36 +0900

    1005 과제

---
+++ Week6/Bank2/src/main/java/com/nhnacademy/bank/Application.java
@@ -0,0 +1,17 @@
+package com.nhnacademy.bank;
+
+import java.math.BigDecimal;
+
+public class Application {
+
+    private static final Long ACCOUNT_NUMBER = 123L;
+
+    public static void main(String[] args) {
+        System.out.println("Application.main() callstack start");
+        var atm = new Atm();
+
+        boolean rt = atm.deposit(BigDecimal.valueOf(100.1), ACCOUNT_NUMBER);
+
+        System.out.println("Application.main() callstack end " + rt);
+    }
+}
